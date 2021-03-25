bd8b2ce70980e4ecd8da82b372e57065744fb946
khacdatdo <datdk.b19cn170@stu.ptit.edu.vn>
2021-03-25 09:17:06 +0700

    update

--- CPPFIB03.cpp
+++ CPPFIB03.cpp
@@ -1,31 +1,37 @@
-#include<iostream>
-
+#include <iostream>
 using namespace std;
-
-bool check(long long n) {
-    if (n == 0 || n == 1) return true;
+bool check(long long n)
+{
+    if (n == 0 || n == 1)
+        return true;
     long long f = 0, a = 0, b = 1;
-    while (f < n) {
+    while (f < n)
+    {
         f = a + b;
         a = b;
         b = f;
     }
-    if (f == n) return true;
+    if (f == n)
+        return true;
     return false;
 }
-
-main() {
+main()
+{
     int t;
     cin >> t;
-    while (t--) {
+    while (t--)
+    {
         int n;
         cin >> n;
         int mang[n + 1];
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
+        {
             cin >> mang[i];
         }
-        for (int i = 0; i < n; i++) {
-            if (check(mang[i])) {
+        for (int i = 0; i < n; i++)
+        {
+            if (check(mang[i]))
+            {
                 cout << mang[i] << " ";
             }
         }
