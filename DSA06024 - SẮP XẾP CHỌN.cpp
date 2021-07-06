b010e0f20397313f528eb88691801a2894fdc9f8
khacdatdo <datdk.b19cn170@stu.ptit.edu.vn>
2021-07-06 14:21:12 +0700

    Update DSA06024 - SẮP XẾP CHỌN.cpp

--- DSA06024 - SẮP XẾP CHỌN.cpp
+++ DSA06024 - SẮP XẾP CHỌN.cpp
@@ -2,20 +2,25 @@
 using namespace std;
 #define ll long long
 
-int main() {
+int main()
+{
     int n;
     cin >> n;
     int a[n];
-    for (int i = 0; i < n; i++) cin >> a[i];
-    for (int i = 0; i < n - 1; i++) {
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    for (int i = 0; i < n - 1; i++)
+    {
         int min = i;
         for (int j = i + 1; j < n; j++)
-            if (a[j] < a[min]) min = j; 
+            if (a[j] < a[min])
+                min = j;
         int tmp = a[min];
         a[min] = a[i];
         a[i] = tmp;
         cout << "Buoc " << i + 1 << ": ";
-        for  (int j = 0; j < n; j++) cout << a[j] << " ";
+        for (int j = 0; j < n; j++)
+            cout << a[j] << " ";
         cout << endl;
     }
     return 0;
